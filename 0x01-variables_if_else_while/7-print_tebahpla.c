6eb9b764ffec0f5af38e19dc60fd54f76166ece2
muhammed <vy@outlook.cl>
2023-09-08 14:18:35 -0700

    s

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: this script writes the minimalized
+ * alphabet with using putchar only on time in the
+ * entire code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122 ; n >= 97; n--)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
