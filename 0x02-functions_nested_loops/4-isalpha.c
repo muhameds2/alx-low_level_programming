07f3f7db692ada3b321f4df5d301bfd88f7fb25a
muhammed <vy@outlook.cl>
2023-09-12 13:25:38 -0700

    s

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+/**
+ * _isalpha - function that check if c
+ * is alphabetic or not
+ * @c: character to be checked
+ *
+ * Return: 1, if c is alphabetic 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	{
+		if (c >= 'a' && c <= 'z')
+			return (1);
+	}
+	return (0);
+}
