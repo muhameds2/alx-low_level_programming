ec24da50162201cf9c4f14870314b3587b95e1c3
muhammed <vy@outlook.cl>
2023-09-12 15:30:04 -0700

    s

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,12 +19,12 @@ void jack_bauer(void)
 			{
 					if (i == 2 && x == 6)
 						break;
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-					_putchar('0' + x);
-					_putchar('0');
-					_putchar('\n');
+					//_putchar('0' + i);
+					//_putchar('0' + j);
+					//_putchar(':');
+					//_putchar('0' + x);
+					//_putchar('0');
+					//_putchar('\n');
 				
 				for (y = 0; y < 10; y++)
 				{
