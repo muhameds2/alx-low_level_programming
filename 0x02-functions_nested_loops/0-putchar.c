33ee9dcba661d8ef1acddecbbbce6e05ffbfcc23
muhammed <vy@outlook.cl>
2023-09-12 10:08:43 -0700

    s

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,13 @@
 #include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  *
  * Description: script to print '_putchar'
  *		without using stdio.h library
- * @c: the char that will be printed
- * _putchar: print char c
  *
  * Return: Always 0 (Success)
  */
-int _putchar(char);
-
 int main(void)
 {
 	_putchar('_');
@@ -23,9 +20,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
