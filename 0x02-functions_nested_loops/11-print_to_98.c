0dab2115fef1e742bcf875a31ecccafcc3f6e2e1
muhammed <vy@outlook.cl>
2023-09-13 08:13:07 -0700

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,10 +16,10 @@ void print_to_98(int n)
 		{
 			printf("%d", i);
 			if (i != 98)
-				printf(", ");
+				print(", ");
 		}
 	}
-	else if (n > 98)
+	else if (n >= 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
