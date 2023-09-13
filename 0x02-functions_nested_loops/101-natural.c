149c417f363b0b03179525b0b7c42dfd1787d20a
muhammed <vy@outlook.cl>
2023-09-13 13:07:22 -0700

    s

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+/**
+ * sum_of_multiples - print the sum of multiples
+ * of 3 or 5 below 1024
+ *
+ * Return: Always void
+ */
+void sum_of_multiples()
+{
+	int i, j, k, sum = 0;
+
+	for (i = 1; i < 1024; i++)
+	{
+		j = i / 3;
+		k = i / 5;
+		if (((j * 3) == i) || ((k * 5) == i))
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
