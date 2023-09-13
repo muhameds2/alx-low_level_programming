120dcea8ca65378f3b1cd78d6ece3cc45f73f400
muhammed <vy@outlook.cl>
2023-09-13 07:28:43 -0700

    s

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,24 +13,22 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			n1 = i * j;
-			if (n1 < 10)
-			{
+			if (j == 0)
 				_putchar('0' + n1);
-				if (j == 9)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if (n1 >= 10)
+			else if (n1 < 10)
 			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + n1);
+			}
+			else
+			{
+				_putchar(' ');
 				_putchar('0' + (n1 / 10));
 				_putchar('0' + (n1 % 10));
-				if (j == 9)
-					continue;
-				_putchar(',');
-				_putchar(' ');
 			}
+			if (j != 9)
+				_putchar(',');
 		}
 		_putchar('\n');
 	}
