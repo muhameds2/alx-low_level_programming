cc5b973d89c6db8e79725467c8702d8b029e1d6d
muhammed <vy@outlook.cl>
2023-09-13 08:24:24 -0700

    s

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,21 +19,23 @@ void print_to_98(int n)
 				printf(", ");
 		}
 	}
-	else if (n >= 98)
+	else if (n > 0 && n < 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
 			if (i != 98)
 				printf(", ");
 		}
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i >= 98; i--)
+		{
 			printf("%d", i);
-		if (i != 98)
-			printf(", ");
+			if (i != 98)
+				printf(", ");
+		}
 	}
 	printf("\n");
 }
