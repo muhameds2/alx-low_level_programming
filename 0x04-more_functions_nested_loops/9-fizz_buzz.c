aaa1a81b9774da98da0989ed338b59e1d2c4c252
muhammed <vy@outlook.cl>
2023-09-14 13:32:44 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 	{
 		if (i != 1)
 			printf(" ");
-		if (i % 3 == 0)
+		if (((i % 3) == 0) && (!((i % 5) == 0)))
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (((i % 5) == 0) && (!((i % 3) == 0)))
 		{
 			printf("Buzz");
 		}
