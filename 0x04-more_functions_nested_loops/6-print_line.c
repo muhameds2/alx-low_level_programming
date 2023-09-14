6ae78e3c9324e3541d67b9535288305947a524e4
muhammed <vy@outlook.cl>
2023-09-14 12:20:48 -0700

    s

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,11 +11,13 @@ void print_line(int n)
 	int i;
 
 	if (n < 0 || n == 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (i= 0; i < n; i++)
-		       _putchar('_');
+		for (i = 0; i < n; i++)
+			_putchar('_');
 		_putchar('\n');
 	}
 }
