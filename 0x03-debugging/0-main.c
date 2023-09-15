08443ef86cefb4a7d478cf647c010d96db1b78c3
muhammed <vy@outlook.cl>
2023-09-15 04:32:43 -0700

    s

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
  * main - tests function that check if an integer positive or negative
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
