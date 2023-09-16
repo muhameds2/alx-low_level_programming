e615b48616330422774c52f859bc659d3b939ce1
muhammed <vy@outlook.cl>
2023-09-15 19:20:40 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,16 +6,13 @@
 int main(void)
 {
 	int i, j;
-	char a, b;
 
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			a = i / 10 + '0';
-			b = i % 10 + '0';
-			putchar(a);
-			putchar(b);
+			putchar('0' + i / 10);
+			putchar('0' + i % 10);
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
