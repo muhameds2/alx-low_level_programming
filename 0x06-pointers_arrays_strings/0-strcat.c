8df4d777fe3972a6bf3bd3a28500303248b85a36
muhammed <vy@outlook.cl>
2023-09-21 14:16:59 -0700

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,8 +6,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *s = dest;
-
 	while (*dest != '\0')
 	{
 		dest++;
@@ -19,5 +17,5 @@ char *_strcat(char *dest, char *src)
 		dest++;
 	}
 	*dest = '\0';
-	return (s);
+	return (dest);
 }
