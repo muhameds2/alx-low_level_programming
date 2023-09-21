faac27c0c609c305705852131fcb62dea0c28122
muhammed <vy@outlook.cl>
2023-09-21 07:19:14 -0700

    s

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 int _putchar(char c);
 
 void reset_to_98(int *n);
+<<<<<<< HEAD
 
 void swap_int(int *a, int*b);
 
@@ -11,3 +12,5 @@ void _puts(char *str);
 void print_rev(char *s);
 
 void rev_string(char *s);
+=======
+>>>>>>> parent of 0bd83cc... s
