d585f16951975ea1f76d26a0bbf9d60a9451a5bc
muhammed <vy@outlook.cl>
2023-09-21 11:54:24 -0700

    s

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,6 +12,11 @@ char *_strcat(char *dest, char *src)
 	{
 		j = i + 1;
 	}
+	if (*(dest + j) != ' ' || *(src + 0) != ' ')
+	{
+		*(dest + j) = ' ';
+		j++;
+	}
 	for (i = 0; *(src + i) != '\0'; i++)
 	{
 		*(dest + j + i) = *(src + i);
