f3406a5721dac8d3f44ec011abb654eeee2892f6
muhammed <vy@outlook.cl>
2023-09-22 13:18:01 -0700

    s

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+/**
+ * _strncpy - copies n bytes of strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+	char *s = dest;
+
+	for (x =0; x < n; x++)
+	{
+		if (*src != '\0')
+		{
+			*s = *src;
+			src++;
+			s++;
+		}
+		else
+		{
+			*s = '\0';
+			s++;
+		}
+	}
+	return (dest);
+}
